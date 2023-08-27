d93dfd0f0e569b89640ede92281caed7fdcf0273
chaitanya3i <chaitanyaillapu3@gmail.com>
2023-08-27 12:05:47 +0530

    extract characters

--- assignment/dsa/day1/firstfive.js
+++ assignment/dsa/day1/firstfive.js
@@ -1,10 +1,13 @@
-//Write a JavaScript function that hides email addresses to prevent unauthorized access. 
-//Test Data :
-//console.log(protect_email("robin_singh@example.com"));
-//"robin...@example.com"
 
-let email="robin_singh@gmail.com"
-  let splitemail=email.split('@')
-    let hideemail= email.slice(0,6)+'...@'+splitemail[1];
-    console.log(hideemail);
+
+   // Write a JavaScript function to extract a specified number of characters from a string. 
+//Test Data :
+//console.log(truncate_string("Robin Singh",4));
+//"Robi"
+
+let Truncate_string="robin singh"
+let truncate_string=Truncate_string.slice(0,4);
+console.log(truncate_string);
+
+
 
